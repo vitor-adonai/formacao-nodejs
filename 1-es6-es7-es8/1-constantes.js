7b5770a7faed0017d573f9c4dedd0ddf6bf2c286
Vitor Adonai <infinitanet@gmail.com>
2020-08-01 04:15:06 -0300

    Constantes

---
+++ 1-es6-es7-es8/1-constantes.js
@@ -0,0 +1,9 @@
+const database = {
+    user: 'root',
+    password: 'root',
+    database: 'app',
+    conection: 'mysql'
+}
+console.table(database)
+// database = 'localhost' Constante não pode sofrer alterações de valores
+console.log(database)
